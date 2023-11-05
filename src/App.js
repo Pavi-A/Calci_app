131ee3bc37483f7ed8614c657d2e576559e89b56
Pavi-A <139478957+Pavi-A@users.noreply.github.com>
2023-11-05 15:50:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import React,{ useState } from 'react';
+import './App.css';
+
+const App=()=>{
+  const[result, setResult]=useState("hi");
+
+
+  return (
+    
+    <div className="container">
+      
+    </div>
+    
+    
+  );
+}
+
+export default App;
